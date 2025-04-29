78bad9c0ef5731bc0f5122477a9b78caf55a77c4
Seguya-Nicholus <seguyanicholus2015@gmail.com>
2025-04-29 09:45:59 +0300

    added REST API global Exception handling

--- src/main/java/com/codewith/springboot_blog_rest_api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/codewith/springboot_blog_rest_api/exception/GlobalExceptionHandler.java
@@ -20,6 +20,20 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(BlogAPIException.class)
+    public ResponseEntity<ErrorDetails> handleBlogAPIException(BlogAPIException exception, WebRequest webRequest){
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
     //global exceptions
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleGlobalException(Exception exception, WebRequest webRequest){
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
 }
